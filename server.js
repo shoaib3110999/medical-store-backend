856db2be49ad1e5724ed92fe72bf6255c6e161cf
shoaib311099 <pshoaib215@gmail.com>
2026-01-22 13:15:53 +0530

    Code upgrade: performance improve kiya

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
+      "http://localhost:5173",
       "https://tjrcis.vercel.app",
       process.env.FRONTEND_URL
     ].filter(Boolean),
